13b4233660a463a9e789e9cdf79704a97e867c2c
AleCejas <alejandro.06555@gmail.com>
2025-05-05 18:40:28 -0300

    Añadiendo algunas configuraciones

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -6,20 +6,32 @@ dotenv.config({
     path: '.env'
 })
 
+const isProduction = process.env.NODE_ENV === 'production'
 
-export const dataSourceOptions: DataSourceOptions = {
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: ['dist/**/*.entity{.ts,.js}'],
-    migrations: ['dist/migration/*{.ts,.js}'],
-    synchronize: true,
-    // dropSchema: true,
-    logging: false,
-}
+
+export const dataSourceOptions: DataSourceOptions = isProduction
+    ? {
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        migrations: ['dist/migration/*{.ts,.js}'],
+        synchronize: true,
+        logging: false
+    }
+
+    : {
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        migrations: ['dist/migration/*{.ts,.js}'],
+        synchronize: true,
+        // dropSchema: true,
+        logging: false,
+    }
 
 export const postgresDataSourceConfig = registerAs('postgres', () => dataSourceOptions)
 
